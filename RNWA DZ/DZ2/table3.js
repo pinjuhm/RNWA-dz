02bca558e94a0f983917a8acbe4bd44f94ed0f7d
pinjuhm <44995339+pinjuhm@users.noreply.github.com>
2021-03-31 14:55:17 +0200

    Delete RNWA DZ directory

--- RNWA DZ/DZ2/table3.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('td').mouseover( function() {
-        var tekst = $(this).parent().text();
-        
-        $('td').attr('title', tekst)
-          .tooltip('show');
-    });
-});
